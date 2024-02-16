29c5b469e60e9fd45300f5e2dbd33fdab0dccf25
Pascal K <pascalkknde@hotmail.com>
2024-02-16 20:02:26 +0000

    refactored to use module

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -1,39 +1,30 @@
-import cv2
-import mediapipe as mp
 import time
+import cv2
+import HandTrackingModule as ht
 
-cap = cv2.VideoCapture(1)
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+def main():
+    p_time = 0
+    cap = cv2.VideoCapture(1)
+    detector = ht.HandDetector()  # Adjusted track_con to be an integer
 
-pTime = 0
-cTime = 0
+    while True:
+        success, img = cap.read()
+        img = detector.find_hands(img)
+        lm_list = detector.find_position(img)
 
-while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+        if lm_list:
+            print(lm_list[4])
 
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark):
+        c_time = time.time()
+        fps = 1 / (c_time - p_time)
+        p_time = c_time
 
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
-                if id == 2:
-                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+        cv2.imshow("image", img)
+        cv2.waitKey(2)
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-    cv2.imshow("image", img)
-    cv2.waitKey(2)
-
+if __name__ == '__main__':
+    main()
--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,18 +1,18 @@
 import cv2
 import mediapipe as mp
-import time
 
 
 class HandDetector:
-    def __init__(self, mode=False, max_hands=2,  model_complexity=1, detection_con=0.5, track_con=0.5):
+    def __init__(self, mode=False, max_hands=2, model_complexity=1, detection_con=0.5, track_con=0.5):
         self.mode = mode
         self.max_hands = max_hands
-        self. model_complexity= model_complexity
+        self.model_complexity = model_complexity
         self.detection_con = detection_con
         self.track_con = track_con
 
         self.mp_hands = mp.solutions.hands
-        self.hands = self.mp_hands.Hands(self.mode, self.max_hands, model_complexity, self.detection_con, self.track_con)
+        self.hands = self.mp_hands.Hands(self.mode, self.max_hands, model_complexity, self.detection_con,
+                                         self.track_con)
         self.mp_draw = mp.solutions.drawing_utils
         self.results = None
 
@@ -40,30 +40,3 @@ class HandDetector:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
         return lm_list
-
-
-def main():
-    p_time = 0
-    cap = cv2.VideoCapture(1)
-    detector = HandDetector()  # Adjusted track_con to be an integer
-
-    while True:
-        success, img = cap.read()
-        img = detector.find_hands(img)
-        lm_list = detector.find_position(img)
-
-        if lm_list:
-            print(lm_list[4])
-
-        c_time = time.time()
-        fps = 1 / (c_time - p_time)
-        p_time = c_time
-
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-        cv2.imshow("image", img)
-        cv2.waitKey(2)
-
-
-if __name__ == '__main__':
-    main()
